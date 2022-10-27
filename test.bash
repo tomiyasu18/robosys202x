5813f13490df1921b144cf263cf2c27921bb83eb
Hiromu Masuda <s21C1109EW@s.chibakoudai.jp>
2022-10-27 12:51:58 +0900

    tekitou

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 # SPDX-FileCopyrightText: 2022 Hiromu Masuda
 # SPDX-License-Identifier: BSD-3-Clause
 
@@ -13,6 +13,14 @@ res=0
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng ${LINENO}
 
+###STRANGE INPUT ###
+out=$(echo あ | ./plus) 
+[ "$?" = 1 ] || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo | ./plus)
+[ "$?" 1 ] || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
 exit $res
