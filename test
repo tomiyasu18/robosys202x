7fa83bcd83719be71d45522633c7a3fcd8d2326c
Hiromu Masuda <s21C1109EW@s.chibakoudai.jp>
2022-10-27 12:19:08 +0900

    yamada

--- test
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-seq 5 | ./plus
-
---
+++ test.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: 2022 Hiromu Masuda
+# SPDX-License-Identifier: BSD-3-Clause
+
+ng () {
+	echo NG at Line $1
+	res=1
+}
+
+res=0
+
+### I/0 TEST ###
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng ${LINENO}
+
+
+[ "$res" = 0 ] && echo OK
+exit $res
+
+
+
